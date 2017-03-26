74ceafabf95eff2d14487a5d3d49e221e56855d8
Aaron Abrook <aabrook@gmail.com>
2017-03-26 15:47:43 +1000

    Initial test run

--- test/higher_order_functions_test.exs
+++ test/higher_order_functions_test.exs
@@ -2,8 +2,26 @@ defmodule HigherOrderFunctionsTest do
   use ExUnit.Case
   doctest HigherOrderFunctions
 
-  test "it composes" do
+  def square(n) do
+    send self(), {:square, n}
+    n * n
+  end
 
+  def cube(n) do
+    send self(), {:cube, n}
+    n * n * n
+  end
+
+  describe "compose" do
+    test "calls right function first" do
+      func = HigherOrderFunctions.compose(&cube/1, &square/1)
+
+      assert func.(2) == 64
+      {:messages, message} = :erlang.process_info(self(), :messages)
+
+      assert_received {:square, 2}
+      assert_received {:cube, 4}
+    end
   end
 
   test "the truth" do
