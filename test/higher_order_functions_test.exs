44d35954e2d3b73111f75b09622c6aa5dc805597
Aaron Abrook <aabrook@gmail.com>
2017-03-26 16:07:46 +1000

    Introduce a spy

--- test/higher_order_functions_test.exs
+++ test/higher_order_functions_test.exs
@@ -2,7 +2,39 @@ defmodule HigherOrderFunctionsTest do
   use ExUnit.Case
   doctest HigherOrderFunctions
 
+  def square(n) do
+    send self(), {:square, n}
+    n * n
+  end
+
+  def cube(n) do
+    send self(), {:cube, n}
+    n * n * n
+  end
+
+  def spy(func, label) do
+    fn x ->
+      send self(), {
+        String.to_atom(label),
+        %{
+          name: "#{inspect func}",
+          arg: "#{inspect x}"
+        }
+      }
+      func.(x)
+    end
+  end
+
   describe "compose" do
+    test "calls with spy" do
+      sq = spy(&square/1, "the square")
+      cu = spy(&cube/1, "the cube")
+      func = HigherOrderFunctions.compose(cu, sq)
+
+      func.(2)
+      IO.inspect :erlang.process_info(self(), :messages)
+    end
+
     test "calls right function first" do
       sq = fn x ->
         send self(), {:square, x}
@@ -14,6 +46,15 @@ defmodule HigherOrderFunctionsTest do
       end
 
       func = HigherOrderFunctions.compose(cu, sq)
+
+      assert func.(2) == 64
+      {:messages, message} = :erlang.process_info(self(), :messages)
+
+      assert message == [{:square, 2},{:cube, 4}]
+    end
+
+    test "calls func with functions" do
+      func = HigherOrderFunctions.compose(&cube/1, &square/1)
 
       assert func.(2) == 64
       {:messages, message} = :erlang.process_info(self(), :messages)
