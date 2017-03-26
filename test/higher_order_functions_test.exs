a0829d80d0f8485f7d08d5a034d1e8a09b7a2af5
Aaron Abrook <aabrook@gmail.com>
2017-03-26 15:53:44 +1000

    Use anonymous functions

--- test/higher_order_functions_test.exs
+++ test/higher_order_functions_test.exs
@@ -2,19 +2,18 @@ defmodule HigherOrderFunctionsTest do
   use ExUnit.Case
   doctest HigherOrderFunctions
 
-  def square(n) do
-    send self(), {:square, n}
-    n * n
-  end
-
-  def cube(n) do
-    send self(), {:cube, n}
-    n * n * n
-  end
-
   describe "compose" do
     test "calls right function first" do
-      func = HigherOrderFunctions.compose(&cube/1, &square/1)
+      sq = fn x ->
+        send self(), {:square, x}
+        x * x
+      end
+      cu = fn x ->
+        send self(), {:cube, x}
+        x * x * x
+      end
+
+      func = HigherOrderFunctions.compose(cu, sq)
 
       assert func.(2) == 64
       {:messages, message} = :erlang.process_info(self(), :messages)
