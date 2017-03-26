0190287259352aafdbdff90176cac747ccdb59e4
Aaron Abrook <aabrook@gmail.com>
2017-03-26 16:51:35 +1000

    Add a spy that cares about arity

--- test/higher_order_functions_test.exs
+++ test/higher_order_functions_test.exs
@@ -14,14 +14,74 @@ defmodule HigherOrderFunctionsTest do
 
   def spy(func, label) do
     fn x ->
+      result = func.(x)
       send self(), {
         String.to_atom(label),
         %{
           name: "#{inspect func}",
-          args: x
+          called_with: [x],
+          result: result
         }
       }
-      func.(x)
+      result
+    end
+  end
+
+  def spy_arity(2, func, label) do
+    fn x, y ->
+      result = func.(x, y)
+      send self(), {
+        String.to_atom(label),
+        %{
+          name: "#{inspect func}",
+          called_with: [x, y],
+          result: result
+        }
+      }
+      result
+    end
+  end
+
+  def spy_arity(3, func, label) do
+    fn x, y, z ->
+      result = func.(x, y, z)
+      send self(), {
+        String.to_atom(label),
+        %{
+          name: "#{inspect func}",
+          called_with: [x, y, z],
+          result: result
+        }
+      }
+      result
+    end
+  end
+
+  describe "our spy" do
+    test "what does the spy get" do
+      spied = spy_arity(2, &Enum.take/2, "taking")
+      spied.([1, 2, 3], 1)
+      IO.inspect :erlang.process_info(self(), :messages)
+
+      receive do
+        {:taking, message} ->
+          assert Enum.at(message.called_with, 0) == [1, 2, 3]
+        _ -> flunk()
+      end
+    end
+
+    test "a 3 arity spy" do
+      reducer = spy_arity(3, &Enum.reduce/3, "reducing")
+      add = fn x, y -> x + y end
+
+      assert reducer.([1, 2, 3], 0, add) == 6
+
+      receive do
+        {:reducing, message} ->
+          assert message.called_with == [[1, 2, 3], 0, add]
+          assert message.result == 6
+        _ -> flunk()
+      end
     end
   end
 
@@ -32,12 +92,11 @@ defmodule HigherOrderFunctionsTest do
       func = HigherOrderFunctions.compose(cu, sq)
 
       func.(2)
-      IO.inspect :erlang.process_info(self(), :messages)
 
       square_name = "#{inspect &HigherOrderFunctions.square/1}"
       cube_name = "#{inspect &HigherOrderFunctions.cube/1}"
-      assert_received {:"the square", %{args: 2, name: square_name}}
-      assert_received {:"the cube", %{args: 4, name: cube_name}}
+      assert_received {:"the square", %{called_with: [2], name: square_name}}
+      assert_received {:"the cube", %{called_with: [4], name: cube_name}}
     end
 
     test "calls right function first" do
