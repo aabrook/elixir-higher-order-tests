53853616a22c6b80a8846c7231976d65a37e1c6b
Aaron Abrook <aabrook@gmail.com>
2017-03-26 15:06:16 +1000

    Add doctests for compose

--- lib/higher_order_functions.ex
+++ lib/higher_order_functions.ex
@@ -3,22 +3,22 @@ defmodule HigherOrderFunctions do
   Documentation for HigherOrderFunctions.
   """
 
+  @doc ~S"""
+  iex> add_1 = fn x -> x + 1 end
+  ...> add_2 = fn x -> x + 2 end
+  ...> add_3 = HigherOrderFunctions.compose(add_1, add_2)
+  ...> add_3.(10)
+  13
+
+  iex> logger = &IO.inspect/1
+  ...> take_last = fn enum -> Enum.take(enum, -1) end
+  ...> take_and_log_last = HigherOrderFunctions.compose(logger, take_last)
+  ...> take_and_log_last.([1, 2, 3])
+  [3]
+  """
   def compose(f, g) do
     fn x ->
       f.(g.(x))
     end
-  end
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> HigherOrderFunctions.hello
-      :world
-
-  """
-  def hello do
-    :world
   end
 end
--- test/higher_order_functions_test.exs
+++ test/higher_order_functions_test.exs
@@ -2,6 +2,10 @@ defmodule HigherOrderFunctionsTest do
   use ExUnit.Case
   doctest HigherOrderFunctions
 
+  test "it composes" do
+
+  end
+
   test "the truth" do
     assert 1 + 1 == 2
   end
